81c7bad36a7f8aa333cf1c8006bc64ce8caf3eb4
tousifk436-coder <tousifk436@gmail.comm>
2026-05-11 10:43:50 +0530

    updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,42 +1,4 @@
 
-
-// import "./globals.css";
-
-// import { AuthProvider } from "@/context/AuthContext";
-// import { CartProvider } from "@/context/CartContext";
-// import { MedicineProvider } from "@/context/MedicineContext";
-
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className="min-h-screen flex flex-col bg-white">
-
-//         <AuthProvider>
-//           <MedicineProvider>
-//             <CartProvider>
-
-//               {/* 🔥 GLOBAL HEADER */}
-//               <Header />
-
-//               {/* PAGE CONTENT */}
-//               <main className="flex-1">
-//                 {children}
-//               </main>
-
-//               {/* 🔥 GLOBAL FOOTER */}
-//               <Footer />
-
-//             </CartProvider>
-//           </MedicineProvider>
-//         </AuthProvider>
-
-//       </body>
-//     </html>
-//   );
-// }
 import "./globals.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
